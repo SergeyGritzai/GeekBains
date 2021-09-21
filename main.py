ca14bd0ed7dac3fdecf0d6d9c6cc263fd64eb9c7
SergeyGritzai <90860530+SergeyGritzai@users.noreply.github.com>
2021-09-22 02:30:29 +0300

    new_1

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# 1 Task
+#Реализовать вывод информации о промежутке времени в зависимости от его продолжительности duration в секундах
+my_time = int(input('Введите время: '))
+print((my_time // 86400), ' дн. ', (my_time % 86400) // 3600, ' ч. ', (my_time % 3600) // 60, ' мин. ', my_time % 60, ' c.')
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('Sergey')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+# 2 Task
+cubes = [x**3 for x in range(1000) if x % 2 != 0]
+
+final_numbers_sum = 0
+# проход по списку
+for num in cubes:
+    numbers_sum = 0
+    for i in str(cubes):
+        numbers_sum += int(numbers_sum)
+    if numbers_sum % 7 == 0:
+        final_numbers_sum += num
+print(final_numbers_sum)
+
+
+cubes = [x**3 + 17 for x in range(1000) if x % 2 != 0]
+
+final_numbers_sum = 0
+# проход по списку
+for num in cubes:
+    numbers_sum = 0
+    for i in str(cubes):
+        numbers_sum += int(numbers_sum)
+    if numbers_sum % 7 == 0:
+        final_numbers_sum += num
+print(final_numbers_sum)
+
+# 3 Task
+percent = int(input('Введите число: '))
+if percent == 1:
+    word = 'процент'
+elif percent <= 4:
+    word = 'процента'
+else:
+    word = 'процентов'
+print(percent, word)
